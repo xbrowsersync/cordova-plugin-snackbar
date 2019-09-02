e0aa1b6ee2af0d8b234762cf3eeb88635cc6e089
nero120 <rich.dmz@protonmail.com>
2019-09-02 23:37:29 +0100

    Fixed plugin already defined error.

--- www/materialSnackbar.js
+++ www/materialSnackbar.js
@@ -1,32 +1,29 @@
-cordova.define("cordova-plugin-snackbar.MaterialSnackbar", function (require, exports, module) {
-    var snackbar = {
-      create: function (text, duration, bgColor, textColor, maxLines, button, successCallback, errorCallback) {
-        cordova.exec(
-          successCallback,
-          errorCallback,
-          'MaterialSnackbar',
-          'show',
-          [{
-            "text": text,
-            "duration": duration || 0,
-            "bgColor": bgColor || '',
-            "textColor": textColor || '',
-            "maxLines": maxLines || 2,
-            "button": button || ''
-          }]
-        );
-      },
-  
-      close: function (successCallback, errorCallback) {
-        cordova.exec(
-          successCallback,
-          errorCallback,
-          'MaterialSnackbar',
-          'hide'
-        );
-      }
+var snackbar = {
+    create: function (text, duration, bgColor, textColor, maxLines, button, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'MaterialSnackbar',
+        'show',
+        [{
+        "text": text,
+        "duration": duration || 0,
+        "bgColor": bgColor || '',
+        "textColor": textColor || '',
+        "maxLines": maxLines || 2,
+        "button": button || ''
+        }]
+    );
+    },
+
+    close: function (successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'MaterialSnackbar',
+        'hide'
+    );
     }
-  
-    module.exports = snackbar;
-  });
-  
+}
+
+module.exports = snackbar;
